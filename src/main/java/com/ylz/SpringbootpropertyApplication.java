68c40f14f9942542a5e2638a87a12a0baccbed3a
yilz <yilz@asiainfo.com>
2021-03-02 09:43:34 +0800

    Initial commit

---
+++ src/main/java/com/ylz/SpringbootpropertyApplication.java
@@ -0,0 +1,13 @@
+package com.ylz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootpropertyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ylz/SpringbootpropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
